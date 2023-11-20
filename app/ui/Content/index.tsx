5694a0e25a0f921d47f637809b7f8e8245283dc6
Zac Tolley <zac@thetolleys.com>
2023-11-20 20:01:56 +0000

    Add a compact variant of content for nested scenarios

--- app/ui/Content/index.tsx
+++ app/ui/Content/index.tsx
@@ -2,6 +2,11 @@ import styles from './styles.module.css'
 
 export const Content: React.FC<{
   children?: React.ReactNode
-}> = ({ children }) => <div className={styles.content}>{children}</div>
+  compact?: boolean
+}> = ({ children, compact }) => (
+  <div className={`${styles.content} ${compact ? styles.compact : ''}`}>
+    {children}
+  </div>
+)
 
 export default Content
