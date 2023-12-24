df3fc85e7a6af452b3cb03ce94186d229c181d78
Zac Tolley <zac@thetolleys.com>
2023-12-24 17:43:48 +0000

    Add code of conduct page

---
+++ app/about/conduct/page.tsx
@@ -0,0 +1,106 @@
+import Content from '@/app/ui/Content'
+import ContentHero from '@/app/ui/ContentHero'
+
+export default function ConductPage() {
+  return (
+    <>
+      <ContentHero image="/banners/socksandboots.jpg" title="Our History" />
+
+      <Content>
+        <h2>Player &amp; Parent Code of Conduct</h2>
+        <h3>Player Code of Conduct </h3>
+        <p>
+          IGA is a non-profit football club that was created as a way to
+          complement girls&apos; grassroots football by offering an advanced
+          set-up that builds positive pathways into women&apos;s football. To
+          achieve this, we operate a &apos;player-centred&apos; approach that
+          delivers expert coaching and guidance on a more individual technical
+          basis, so that every player can learn and develop at their own pace.
+          We create a professional environment where our players are encouraged
+          to experiment and be creative and have no fear of failure in order to
+          develop their game.
+        </p>
+        <p> As an Inspire Girls Academy player, I will... </p>
+        <ul>
+          <li>Be punctual for training and matches </li>
+          <li>
+            Be prepared by showing up with correct kit, equipment and footwear
+          </li>
+          <li>Always play and train at my best for the benefit of the team</li>
+          <li>
+            Play fairly - I won&apos;t cheat, dive, complain or waste time
+          </li>
+          <li>Respect the facilities and equipment, both home and away </li>
+          <li>
+            Offer my support and gratitude to the opposition - win or lose
+          </li>
+          <li>Listen and respond to my coach&apos;s instructions/decisions </li>
+          <li>
+            Talk to someone at the Club who I trust if I&apos;m unhappy or
+            worried about any of my team mates
+          </li>
+          <li>
+            Enjoy my football and adopt a positive outlook and approach to
+            playing
+          </li>
+          <li>
+            Be mindful and respectful of how I talk about football activity and
+            the Club on social media
+          </li>
+          <li>
+            RESPECT all on the pitch - team-mates, opposition, the referee and
+            the team coaches/management
+          </li>
+        </ul>
+        <h3>Parent Code of Conduct </h3>
+        <p>
+          As a parent/guardian/spectator associated with Inspire Girls Academy
+          player, I will...
+        </p>
+        <ul>
+          <li>
+            Encourage my player to be punctual for all training sessions and
+            matches
+          </li>
+          <li>
+            Ensure my player is prepared with the right kit, footwear and
+            equipment Keep up to date with club communications (Heja, emails)
+          </li>
+          <li>
+            Keep the Club informed of any medical conditions/family situations
+            so we can provide appropriate support
+          </li>
+          <li>
+            Be mindful and respectful of how I talk about football activity and
+            the Club on social medi
+          </li>
+          <li>
+            Applaud effort and good play as well as success Respect the
+            Referee&apos;s decisions even when I don&apos;t agree with them
+          </li>
+          <li>Appreciate good play from whatever team it comes from </li>
+          <li>
+            Remain behind the touchline and within the Designated Spectators’
+            Area
+          </li>
+          <li>
+            Let the coach do their job and don&apos;t confuse the players by
+            telling them what to do (buzz words such as press, drop, turn etc.)
+          </li>
+          <li>
+            Encourage the players to respect the opposition, referee and match
+            officials
+          </li>
+          <li>
+            Support positively and offer players encouragement not criticism
+          </li>
+          <li>
+            Never engage in, or tolerate offensive, insulting or abusive
+            language or behaviour
+          </li>
+          <li>Be a positive role model for my player</li>
+        </ul>
+      </Content>
+    </>
+  )
+}
